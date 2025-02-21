4aad67b430d7ac13894b384587b7583e432e798e
krityagi <ktyagi0602@gmail.com>
2025-02-21 17:17:25 +0000

    Updated

--- auth.js
+++ auth.js
@@ -7,15 +7,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const authRoutes = require('./routes/authRoutes').router;
+const authRoutes = require('./routes/auth').router; 
+const adminRoutes = require('./routes/admin'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const MONGO_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mongo:27017/authService?authSource=admin`;
+// MongoDB connection string with authentication
+const MONGO_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mongo:27017/devopsduniya?authSource=admin`;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected for Auth Service'))
+    .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error: ', err));
 
 app.set('view engine', 'ejs');
@@ -32,9 +34,32 @@ app.use(session({
     cookie: { secure: false }
 }));
 
-// Routes
-app.use(authRoutes);
+// Middleware to make user data available in all templates
+app.use((req, res, next) => {
+    res.locals.user = req.session.user; // Make user available in templates
+    next();
+});
+
+// Health check endpoint
+app.get('/healthz', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Readiness check endpoint
+app.get('/readiness', (req, res) => {
+    res.status(200).send('Ready');
+});
+
+// Add a route to handle GET requests to the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page!');
+});
+
+app.use(authRoutes); // Use authRoutes directly without `.router`
+app.use('/admin', adminRoutes); // adminRoutes is already exporting the router
+
+console.log('Admin routes added'); 
 
 app.listen(port, () => {
-    console.log(`Auth Service is running on http://localhost:${port}/login`);
+    console.log(`Server is running on http://localhost:${port}/login`);
 });
