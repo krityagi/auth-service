c2560cc947567b02ea2bfa32db20511a76d520d4
krityagi <ktyagi0602@gmail.com>
2025-03-07 11:41:57 +0000

    updated auth.js

--- auth.js
+++ auth.js
@@ -5,8 +5,6 @@ const session = require('express-session');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const axios = require('axios');
-const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
 
 
 dotenv.config();
@@ -24,13 +22,6 @@ const MONGO_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWOR
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected for Auth Service'))
     .catch(err => console.log('MongoDB connection error: ', err));
-
-redisClient.on('connect', () => {
-    console.log('Redis client connected');
-});
-redisClient.on('error', (err) => {
-    console.log('Redis client error:', err);
-});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
