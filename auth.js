494b0e7a6e8aa9cdb8224806f9149d65143a205a
krityagi <ktyagi0602@gmail.com>
2025-03-09 01:56:07 +0000

    updated

--- auth.js
+++ auth.js
@@ -45,6 +45,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log('SESSION_SECRET:', process.env.SESSION_SECRET);
 app.use(session({
     store: new RedisStore({ client: redisClient }),
     secret: process.env.SESSION_SECRET,
